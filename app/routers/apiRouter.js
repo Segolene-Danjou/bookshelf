96e76bd620ce06acb431f3ae9510aeeb89900844
Segolene-Danjou <segolene.danjou@gmail.com>
2021-05-27 19:45:42 +0200

    mise en place des routes

--- app/routers/apiRouter.js
+++ app/routers/apiRouter.js
@@ -31,23 +31,6 @@ router.route('/authors')
      */
     .post(validate.body(schemas.authorInsertSchema), controllerFactory.add('author'));
 
-    router.route('/books')
-    /**
-     * Liste des auteurs
-     * @route GET /authors
-     * @returns {Author[]} 200 - La liste des auteurs
-     * @returns {Error} 500 - Une erreur serveur
-     */
-    .get(controllerFactory.getAll('book'))
-    /**
-     * Ajouter un auteur
-     * @route POST /authors
-     * @param {AuthorInput.model} Author.body.required - Un objet contenant les informations d'un auteur
-     * @returns {Author} 200 - L'auteur créer
-     * @returns {Error} 500 - Auteur déjà présent dans la BDD
-     */
-    .post(controllerFactory.add('book'));
-
 router.route('/authors/:id(\\d+)')
     /**
      * Un auteur
@@ -75,6 +58,52 @@ router.route('/authors/:id(\\d+)')
      */
     .delete(controllerFactory.delete('author'));
 
+router.route('/books')
+    /**
+     * Liste des auteurs
+     * @route GET /books
+     * @returns {Author[]} 200 - La liste des auteurs
+     * @returns {Error} 500 - Une erreur serveur
+     */
+    .get(controllerFactory.getAll('book'))
+    /**
+     * Ajouter un auteur
+     * @route POST /books
+     * @param {AuthorInput.model} Author.body.required - Un objet contenant les informations d'un auteur
+     * @returns {Author} 200 - L'auteur créer
+     * @returns {Error} 500 - Auteur déjà présent dans la BDD
+     */
+    .post(controllerFactory.add('book'));
+
+router.route('/books/:id(\\d+)')
+    /**
+     * Un livre
+     * @route GET /books/{id}
+     * @param {number} id - Identifiant du livre
+     * @returns {Book.model} 200 - un livre
+     * @returns {Error} 500 - Une erreur serveur
+     */
+    .get(bookController.getById)
+    /**
+     * Mise à jour d'un livre
+     * @route PATCH /books/{id}
+     * @param {number} id - Identifiant du livre
+     * @param {BookInput.model} Book.body.required - Un objet  contenant les informations partiels d'un livre
+     * @returns {Book.model} 200 - un livre créer
+     * @returns {Error} 500 - Une erreur serveur
+     */
+    .patch(validate.body(schemas.bookUpdateSchema), controllerFactory.update('book'))
+    /**
+     * Un livre
+     * @route DELETE /books/{id}
+     * @param {number} id - Identifiant du livre
+     * @returns {Book} 204 - <empty content>
+     * @returns {Error} 500 - Une erreur serveur
+     */
+    .delete(controllerFactory.delete('book'));
+
+
+
 router.route('/publishers')
     /**
      * Liste des éditeurs
@@ -91,6 +120,33 @@ router.route('/publishers')
      * @returns {Error} 500 - Éditeur déjà présent dans la BDD
      */
     .post(validate.body(schemas.publisherInsertSchema), controllerFactory.add('publisher'));
+
+router.route('/publishers/:id(\\d+)')
+    /**
+     * une maison d'édition
+     * @route GET /publishers/{id}
+     * @param {number} id - Identifiant de la maison d'édition
+     * @returns {Publisher.model} 200 - la maison d'édition
+     * @returns {Error} 500 - Une erreur serveur
+     */
+    .get(publisherController.getById)
+    /**
+     * Mise à jour d'une maison d'édition
+     * @route PATCH /publishers/{id}
+     * @param {number} id - Identifiant de la maison d'édition
+     * @param {PublisherInput.model} Publisher.body.required - Un objet  contenant les informations partiels d'une maison d'édition
+     * @returns {Publisher.model} 200 - la maison d'édition créer
+     * @returns {Error} 500 - Une erreur serveur
+     */
+    .patch(validate.body(schemas.publisherUpdateSchema), controllerFactory.update('publisher'))
+    /**
+     * une maison d'édition
+     * @route DELETE /publishers/{id}
+     * @param {number} id - Identifiant de la maison d'édition
+     * @returns {Publisher} 204 - <empty content>
+     * @returns {Error} 500 - Une erreur serveur
+     */
+    .delete(controllerFactory.delete('publisher'));
 
 
 router.route('/genre')
@@ -109,6 +165,33 @@ router.route('/genre')
      * @returns {Error} 500 - Genre déjà présent dans la BDD
      */
      .post(validate.body(schemas.genreInsertSchema), controllerFactory.add('genre'));
+
+router.route('/genres/:id(\\d+)')
+     /**
+      * Un genre
+      * @route GET /genres/{id}
+      * @param {number} id - Identifiant du genre
+      * @returns {Genre.model} 200 - Le genre
+      * @returns {Error} 500 - Une erreur serveur
+      */
+     .get(genreController.getById)
+     /**
+      * Mise à jour d'un genre
+      * @route PATCH /genres/{id}
+      * @param {number} id - Identifiant du genre
+      * @param {GenreInput.model} Genre.body.required - Un objet  contenant les informations partiels d'un genre
+      * @returns {Genre.model} 200 - Le genre créer
+      * @returns {Error} 500 - Une erreur serveur
+      */
+     .patch(validate.body(schemas.genreUpdateSchema), controllerFactory.update('genre'))
+     /**
+      * Un genre
+      * @route DELETE /genres/{id}
+      * @param {number} id - Identifiant du genre
+      * @returns {Genre} 204 - <empty content>
+      * @returns {Error} 500 - Une erreur serveur
+      */
+     .delete(controllerFactory.delete('genre'));
     
 // Resource Not Found
 router.use(errorController.resourceNotFound);
